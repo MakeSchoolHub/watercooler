bdff3126230f96e457b49df1ca42ea6bfa8e367d
Jonasz Karwacki <jonaszakr@gmail.com>
2020-10-05 23:53:03 -0700

    Convert env vars to strings

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,9 +4,9 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    apiKey: process.env.FIREBASE_API_KEY,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+    apiKey: `${process.env.FIREBASE_API_KEY}`,
+    databaseURL: `${process.env.FIREBASE_DATABASE_URL}`
   };
 firebase.initializeApp(config);
 
