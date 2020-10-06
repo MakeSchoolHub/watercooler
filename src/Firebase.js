a7a545483a71bbfc78e62c5716b31e4b545c2b76
Jonasz Karwacki <jonaszakr@gmail.com>
2020-10-06 01:28:00 -0700

    Use hardcoded url temporarily.

--- src/Firebase.js
+++ src/Firebase.js
@@ -6,8 +6,10 @@ const settings = {timestampsInSnapshots: true};
 const config = {
     projectId: `${process.env.FIREBASE_PROJECT_ID}`,
     apiKey: `${process.env.FIREBASE_API_KEY}`,
-    databaseURL: `${process.env.FIREBASE_DATABASE_URL}`
+    databaseURL: "https://bonfire-3204e.firebaseio.com/"
+    //databaseURL: `${process.env.FIREBASE_DATABASE_URL}`
   };
+console.log('safksjfdhkdsljhflskjhf');
 firebase.initializeApp(config);
 
 firebase.firestore().settings(settings);
